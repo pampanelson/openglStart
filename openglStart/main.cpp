4ef0f2e417d4170d09ab8a4fcc6d4a07e0a6fd99
Pampa Nelson <pampa.nelson@gmail.com>
2019-07-26 09:07:57 +0800

    Initial commit

---
+++ openglStart/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openglStart
+//
+//  Created by Pampa Nie on 2019/7/26.
+//  Copyright © 2019 Pampa Nie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
