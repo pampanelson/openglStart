f109fc4a83b8123a9d25f664fd16003a5e36cb0f
Pampa Nelson <pampa.nelson@gmail.com>
2019-07-26 09:17:41 +0800

    set up ready for tamplate

--- openglStart/main.cpp
+++ openglStart/main.cpp
@@ -6,10 +6,38 @@
 //  Copyright © 2019 Pampa Nie. All rights reserved.
 //
 
-#include <iostream>
+#ifdef __APPLE__
+/* Defined before OpenGL and GLUT includes to avoid deprecation messages */
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glut.h>
+#endif
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(-0.5, 0.5);
+    glVertex2f(0.5, 0.5);
+    glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
+}
+int main(int argc, char ** argv)
+{
+    glutInit(&argc, argv);
+    glutCreateWindow("Xcode Glut Demo");
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
